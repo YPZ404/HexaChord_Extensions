ad4ac091b49627be60bf4a31d8975d6553af6172
michael1699 <74026209+michael1699@users.noreply.github.com>
2023-08-07 14:12:41 +1000

    Update run_HexaChord.sh

--- run_HexaChord.sh
+++ run_HexaChord.sh
@@ -1,1 +1,1 @@
-/Users/yupeng/.sdkman/candidates/java/current/bin/java -ea -Dfile.encoding=UTF-8 -classpath bin:lib/iText-2.1.5.jar:lib:lib/javaplex-4.1.0.jar:lib/jcommon-1.0.16.jar:lib/jfreechart-1.0.13.jar:lib/gluegen-rt-cdc.jar:lib/gluegen-rt.jar:lib/jogl.all.jar Main.HexaChord
+/bin/java -ea -Dfile.encoding=UTF-8 -classpath bin:lib/iText-2.1.5.jar:lib:lib/javaplex-4.1.0.jar:lib/jcommon-1.0.16.jar:lib/jfreechart-1.0.13.jar:lib/gluegen-rt-cdc.jar:lib/gluegen-rt.jar:lib/jogl.all.jar Main.HexaChord
